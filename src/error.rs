ee72c03be62ad40c374e32fd67122f612d35973c
marco <toyko2001@gmail.com>
2024-05-30 09:37:08 +0630

    add error mode, and testing

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,21 @@
+use axum::response::IntoResponse;
+use reqwest::StatusCode;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Debug)]
 pub enum Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR").into_response()
+    }
+}
---
+++ tests/fetch_movies.rs
@@ -0,0 +1,2 @@
+#[test]
+fn fetch_many_videos() {}
